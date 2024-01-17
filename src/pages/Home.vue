a58ab01843af49330296e3a593b9fd1c17354018
WildanYR <wildanyahya99@gmail.com>
2024-01-18 06:57:54 +0700

    style: home page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,27 +1,49 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+
+const routes = ref([
+  {
+    name: "Fitur",
+    routes: [
+      { to: "UserList", name: "User" },
+      { to: "PatientList", name: "Pasien" },
+    ],
+  },
+  {
+    name: "Data",
+    routes: [
+      { to: "ClinicList", name: "Klinik" },
+      { to: "TreatmentPacketList", name: "Paket Perawatan" },
+      { to: "TherapyList", name: "Terapi (Tidakan)" },
+      { to: "MeridianList", name: "Meridian" },
+      { to: "ComplaintList", name: "Keluhan" },
+      { to: "MedicineList", name: "Riwayat Minum Obat" },
+      { to: "DoctorDiagnosisList", name: "Diagnosa Dokter" },
+      { to: "SelfTherapyList", name: "Terapi Mandiri" },
+      { to: "StomachCheckupList", name: "Pemeriksaan Perut" },
+      { to: "TongueCheckupList", name: "Pemeriksaan Lidah" },
+      { to: "DurationAdviceList", name: "Saran Perawatan" },
+    ],
+  },
+]);
+</script>
 
 <template>
-  <div class="flex flex-col">
-    <RouterLink :to="{ name: 'PatientList' }">Pasien</RouterLink>
-    <RouterLink :to="{ name: 'ClinicList' }">Klinik</RouterLink>
-    <RouterLink :to="{ name: 'MedicineList' }">Obat</RouterLink>
-    <RouterLink :to="{ name: 'DoctorDiagnosisList' }">
-      Diagnosa Dokter
-    </RouterLink>
-    <RouterLink :to="{ name: 'SelfTherapyList' }"> Terapi Mandiri </RouterLink>
-    <RouterLink :to="{ name: 'TreatmentPacketList' }"
-      >Paket Perawatan
-    </RouterLink>
-    <RouterLink :to="{ name: 'StomachCheckupList' }"
-      >Pemeriksaan Perut
-    </RouterLink>
-    <RouterLink :to="{ name: 'TongueCheckupList' }"
-      >Pemeriksaan Lidah
-    </RouterLink>
-    <RouterLink :to="{ name: 'TherapyList' }">Terapi (tindakan) </RouterLink>
-    <RouterLink :to="{ name: 'MeridianList' }">Meridian</RouterLink>
-    <RouterLink :to="{ name: 'ComplaintList' }">Keluhan</RouterLink>
-    <RouterLink :to="{ name: 'UserList' }">User</RouterLink>
-    <RouterLink :to="{ name: 'DurationAdviceList' }">Saran Terapi</RouterLink>
+  <div class="container p-10 mx-auto">
+    <div class="space-y-8">
+      <div v-for="(group, i) in routes" :key="'route-' + i">
+        <h2 class="mb-3 text-2xl font-medium">{{ group.name }}</h2>
+        <div class="grid grid-cols-4 gap-5 justify-stretch">
+          <RouterLink
+            v-for="(route, i) in group.routes"
+            :key="'route-' + i"
+            :to="{ name: route.to }"
+            class="w-full px-6 py-5 text-lg font-medium text-center border border-gray-300 rounded-md hover:bg-blue-500 hover:text-white"
+          >
+            {{ route.name }}
+          </RouterLink>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
