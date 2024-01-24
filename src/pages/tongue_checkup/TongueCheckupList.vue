8ca6411e0ac083d278024eadea1a0a95f6488452
WildanYR <wildanyahya99@gmail.com>
2024-01-24 15:18:57 +0700

    style: change action dropdown to button

--- src/pages/tongue_checkup/TongueCheckupList.vue
+++ src/pages/tongue_checkup/TongueCheckupList.vue
@@ -14,12 +14,8 @@ import TableRowBody from "../../components/tables/TableRowBody.vue";
 import TableBody from "../../components/tables/TableBody.vue";
 import Pagination from "../../components/Pagination.vue";
 import EmptyData from "../../components/EmptyData.vue";
-import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue";
-import ThreeDotIcon from "../../components/icon/ThreeDotIcon.vue";
 import { useRouter } from "vue-router";
 import LoadingButton from "../../components/LoadingButton.vue";
-import EditIcon from "../../components/icon/EditIcon.vue";
-import TrashIcon from "../../components/icon/TrashIcon.vue";
 import TextSearch from "../../components/form/TextSearch.vue";
 import ConfirmDialog from "../../components/dialog/ConfirmDialog.vue";
 import GrayButton from "../../components/button/GrayButton.vue";
@@ -190,68 +186,19 @@ onMounted(() => {
               :key="'TongueCheckup-table-col-' + j"
               >{{ col }}
             </TableBody>
-            <TableBody class="flex items-center justify-end">
-              <Menu as="div" class="relative inline-block text-left">
-                <div>
-                  <MenuButton
-                    class="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100"
-                  >
-                    <ThreeDotIcon class="w-5 h-5"></ThreeDotIcon>
-                  </MenuButton>
-                </div>
-
-                <transition
-                  enter-active-class="transition duration-100 ease-out"
-                  enter-from-class="transform scale-95 opacity-0"
-                  enter-to-class="transform scale-100 opacity-100"
-                  leave-active-class="transition duration-75 ease-in"
-                  leave-from-class="transform scale-100 opacity-100"
-                  leave-to-class="transform scale-95 opacity-0"
-                >
-                  <MenuItems
-                    class="absolute right-0 z-30 w-56 mt-2 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black/5 focus:outline-none"
-                  >
-                    <div class="px-1 py-1">
-                      <MenuItem v-slot="{ active }">
-                        <button
-                          @click="
-                            handleOnTongueCheckupEdit(TongueCheckups[i].id)
-                          "
-                          :class="[
-                            active
-                              ? 'bg-violet-500 text-white'
-                              : 'text-gray-900',
-                            'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                          ]"
-                        >
-                          <EditIcon
-                            class="w-5 h-5 mr-2 text-violet-400"
-                            aria-hidden="true"
-                          />
-                          Edit
-                        </button>
-                      </MenuItem>
-                      <MenuItem v-slot="{ active }">
-                        <button
-                          @click="openDeleteModal(TongueCheckups[i].id)"
-                          :class="[
-                            active
-                              ? 'bg-violet-500 text-white'
-                              : 'text-gray-900',
-                            'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                          ]"
-                        >
-                          <TrashIcon
-                            class="w-5 h-5 mr-2 text-violet-400"
-                            aria-hidden="true"
-                          />
-                          Hapus
-                        </button>
-                      </MenuItem>
-                    </div>
-                  </MenuItems>
-                </transition>
-              </Menu>
+            <TableBody class="flex items-center gap-3">
+              <button
+                @click="handleOnTongueCheckupEdit(TongueCheckups[i].id)"
+                class="flex items-center gap-1 px-4 py-1 text-sm rounded-md text-amber-900 bg-amber-100 hover:bg-amber-200 group"
+              >
+                Edit
+              </button>
+              <button
+                @click="openDeleteModal(TongueCheckups[i].id)"
+                class="flex items-center gap-1 px-4 py-1 text-sm text-red-900 bg-red-100 rounded-md hover:bg-red-200 group"
+              >
+                Hapus
+              </button>
             </TableBody>
           </TableRowBody>
         </template>
